426210615cdb80590ab1ce130337c8659a9b27be
Zerol Acqua <68687848+ZerolAcqua@users.noreply.github.com>
2020-08-24 23:10:05 +0800

    Update run.py

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ import os
 
 f = open('run_manim.bat', 'w')
 
-py_file_name = 'Sudoku.py'
-classname = 'Sudoku_start'
+py_file_name = 'Show_Sudoku.py'
+classname = 'Sudoku_basic'
 pl = ' -pl'
 pm = ' -pm'
 str01 = 'python -m manim ' + py_file_name + ' ' + classname + pl
