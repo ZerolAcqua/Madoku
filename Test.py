0d9d8b6504ad3c04be1e6a32271c5015b50d95de
Acqua <hlc118@outlook.com>
2021-07-25 11:35:25 +0800

    对Sudoku类进行了部分修改，添加Sudoku_universe，添加了部分测试例

--- Test.py
+++ Test.py
@@ -43,6 +43,12 @@ class TestScene(Scene):
         self.play(Transform(Sudoku1,Sudoku3))
         self.wait()
 
+        # self.play(FadeOut(Sudoku1))
+        # Sudoku2=Sudoku1.copy()
+        # Sudoku2.shift(RIGHT*2)
+        # self.play(Write(Sudoku1))
+        # self.play(Write(Sudoku2))
+
 class TestScene1(Scene):
     CONFIG = {
         "camera_config": {
@@ -52,3 +58,93 @@ class TestScene1(Scene):
     def construct(self):
         Sl1=SquareLoop(color=PURPLE)
         self.play(Write(Sl1))
+
+
+class TestScene2(Scene):
+    def construct(self):
+        self.num_str='1..4..6..2..1.....5...7..9.......9.8....647.........5.62...3..443.85..6...9..1..7'
+        temp = SudokuLine(self.num_str[9 * 1 + 1 - 10], color=ORANGE,
+                          plot_depth=2, size=1)
+        self.play(Write(temp))
+
+
+class TestScene3(Scene):
+    def construct(self):
+        title=Title("候选数初步.....",match_underline_width_to_text=True).move_to(UP*2.5)
+        contents=BulletedList(
+                "唯余",
+                "区块排除",
+                "死锁/互补思路",
+                "数对数组（显性隐性）",
+                "两种流派的辨析"
+        ).next_to(title,DOWN)
+
+        title.set_color(WHITE)
+        self.wait()
+        self.play(Write(title))
+        self.wait()
+        self.play(Write(contents))
+
+class TestScene4(Scene):
+    CONFIG = {
+        "camera_config": {
+            "background_color": WHITE,
+        },
+    }
+    def construct(self):
+        # 这里是要显示的文字
+        hidden_subset=TextMobject("隐性数组",plot_depth=2,background_stroke_color=WHITE)
+        self.play(ShowCreation(hidden_subset))
+
+        # 这里是文字的背景矩形
+        # hidden_subset.add_background_rectangle(opacity=0)
+        hidden_subset.add_background_rectangle()
+        hidden_subset.background_rectangle.set_opacity(0)
+        rectangle= hidden_subset.background_rectangle.copy()
+        # 把矩形不透明度改为1，
+        rectangle.shift(LEFT*3+UP*0.1).set_plot_depth(1).set_fill(color=BLUE)
+        rectangle.set_opacity(1)
+
+        # 下面是将最终位置变到文字所在处的动画（关系不大）
+        self.play(FadeIn(rectangle))
+        self.play(rectangle.shift,RIGHT*2)
+        self.play(rectangle.shift,RIGHT*2+DOWN*0.5)
+        self.play(rectangle.move_to,hidden_subset.get_center())
+        self.wait()
+
+class TestScene5(Scene):
+    # CONFIG = {
+    #     "camera_config": {
+    #         "background_color": WHITE,
+    #     },
+    # }
+    def construct(self):
+        text=Text("1234",plot_depth=1)
+        square=Square(fill_color=BLUE,fill_opacity=1,stroke_color=RED)
+        self.play(FadeIn(text))
+        self.play(FadeIn(square))
+        self.wait()
+        self.play(FadeIn(text))
+        print(text.get_plot_depth(),square.get_plot_depth())
+        self.wait()
+
+class TestScene6(Scene):
+    CONFIG = {
+        "camera_config": {
+            "background_color": GRAY,
+        },
+    }
+    def construct(self):
+        print(Sudoku.rows_index_list)
+        print(Sudoku.cols_index_list)
+        print(Sudoku.blocks_index_list)
+        print(Sudoku.except_list)
+
+        sudoku1 = Sudoku(num_str = '1..4..6..2..1.....5...7..9.......9.8....647.........5.62...3..443.85..6...9..1..7',center_of_squares=3.5*LEFT)
+        self.play(Write(sudoku1))
+        sudoku2 = Sudoku(num_str='.314...78...1.........6...34....1.3..7.6.9.5..2.3....97...2.........6...69...542.',center_of_squares=3.5*RIGHT)
+        self.play(Write(sudoku2))
+        self.wait(1)
+        self.play(sudoku1.init_cands.shift,RIGHT)
+
+
